5861286292b3824019795b925ba2e37b4e1aea8c
Patrick Bryan <patrickmbryan@gmail.com>
2015-03-22 18:25:44 -0400

    test

--- Compilers/final/cflatc.c
+++ Compilers/final/cflatc.c
@@ -7,6 +7,7 @@
 #include "cflatc.h"
 #include "lexxer.h"
 
+extern FILE *yyin;
 FILE *sourceFile;
 char* input;
 
--- Compilers/final/cflatc.h
+++ Compilers/final/cflatc.h
@@ -9,9 +9,6 @@
 #include <stdio.h>
 #include <string.h>
 
-extern FILE *yyin;
-extern yyparse();
-
 int fileOpened(char* fileName);
 
 void compile(void);
